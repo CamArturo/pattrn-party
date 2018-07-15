9deaa2b92141dd1885f41ed706b4cd22408dabcc
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-15 15:36:56 -0600

    Finished working accordion

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const accordion = $(".accordion");
+
+$(function () {
+  accordion.children("li").first().children("a").addClass("active")
+    .next().addClass("is-open").show();
+
+  accordion.on("click", "li > a", function () {
+    if (!$(this).hasClass("active")) {
+
+      console.log("clicked");
+      // hide the previous tab with class "is-open"
+      $(".accordion .is-open").removeClass("is-open").hide();
+
+      // find the next sibling (paragraph section) and toggle 'is-open'
+      $(this).next().toggleClass("is-open").toggle();
+
+      // find active remove it
+      $(".accordion").find(".active").removeClass("active");
+
+      // reassign this li class of active
+      $(this).addClass("active");
+    }
+    // else {
+    //   $(".accordion .is-open").removeClass("is-open").hide();
+    //   $(this).removeClass("active");
+    // }
+  });
+});
