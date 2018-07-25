62876308a22f975a7e63d6f5dddd152cc48bdb4c
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-25 14:13:49 -0600

    Fix accordion tabs, fixed wrong grey colors, clean up;

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
-const accordion = $(".accordion");
+const accordion = $(".accordion-container");
 
 $(function () {
   accordion.children("li").first().children("a").addClass("active")
     .next().addClass("is-open").show();
 
   accordion.on("click", "li > a", function () {
+
     if (!$(this).hasClass("active")) {
 
-      console.log("clicked");
       // hide the previous tab with class "is-open"
-      $(".accordion .is-open").removeClass("is-open").hide();
+      $(".accordion-container .is-open").removeClass("is-open");
+
+      //.hide()
 
       // find the next sibling (paragraph section) and toggle 'is-open'
       $(this).next().toggleClass("is-open").toggle();
 
       // find active remove it
-      $(".accordion").find(".active").removeClass("active");
+      $(".accordion-container").find(".active").removeClass("active");
 
       // reassign this li class of active
       $(this).addClass("active");
     }
-    // else {
-    //   $(".accordion .is-open").removeClass("is-open").hide();
-    //   $(this).removeClass("active");
-    // }
+    else {
+      $(".accordion-container .is-open").removeClass("is-open");
+      $(this).removeClass("active");
+    }
   });
-});
 
-$(function () {
+  if (!Modernizr.mq("(max-width: 768px)")) {
+    let combinedHeight = $('.accordion').height();
+    $(".main-container > .btn-container").css("margin-top", combinedHeight + 150);
+  }
+
+  $(window).on("resize", function () {
+    let combinedHeight;
+    if (!Modernizr.mq("(max-width: 768px)")) {
+      combinedHeight = $('.accordion').height();
+      $(".main-container > .btn-container").css("margin-top", combinedHeight + 150);
+    }
+  });
+
+  // Header
   $(".header-bar-container").addClass("js");
 
   $(".mobile-nav").on("click", (e) => {
@@ -35,5 +49,4 @@ $(function () {
     $(".menu-link").toggleClass("active");
     $("#menu").toggleClass("active");
   });
-
 });
